ddb4d29bd77089a1bd0aa17d264c53e438fb6284
juergenschmidt <jurgenschmidt@Jurgens-iMac.local>
2014-11-19 11:35:24 +0100

    T2
    functions (private/inner) (private/outer)
    Ein options-Array als Propertie-Container

--- script/japs.js
+++ script/japs.js
@@ -1,13 +1,35 @@
 (function (global, undefined) {
 
+// offene Fragen:
+// Parameter : Value vs. Referenz z.B. init der Konstanten
 
 
+        ///////////////////////////////////////////////////////////
+        //
+        // private methods
+        //
+        // private Methode !ausserhalb! des Constructors definiert.
+        // Vorteil: EINE funktion für alle Instanzen des Objektes
+        // Zugriff auf properties durch Parameter simuliert
+        //
+        ///////////////////////////////////////////////////////////
+        function privateMethod() {
+
+        }; // privateMethod
+
+        // Hilfsroutinen
+        function test(o) {
+            debugger;
+            //var x1 = that.container;
+            var x2 = o.gates;
+            var x3 = 'neu';
+            o.mainContainerSelector = x3;
+     //       console.log(that);
+            console.log(o);
+        }
 
 // Was zu tun ist, wenn this.ratio sich ändert
 
-// pivate Methode !ausserhalb! des Constructors definiert.
-// Vorteil: EINE funktion für alle Instanzen des Objektes
-// Zugriff auf properties durch Parameter simuliert
         function calcRatio(orient, ratio, container) {
             var v,  // rechenspeicher der ratio
                 r,  // seitenverhältnis in %
@@ -26,7 +48,27 @@
             } // portrait
         } // calcRatio
 
+        function calcWindowWidths(proz_thumbs, mainpictureWidthLandscape ) {
+            var b,  // neue breite
+                p   // stringrepresentation
+            b = 100-proz_thumbs;
+            p = b.toString()+'%'
+            mainpictureWidthLandscape = p;
+            // short
+            mainpictureWidthLandscape = (100-proz_thumbs)+'%';
+        }
 
+        // Was zu tun ist, wenn this.proz_thumbs sich ändert
+        function calcThumbList(container, height_thumbs, proz_thumbs) {
+            var t, // container der thumbs
+                p  // stringrepräsentation von height_thumbs
+            /* portrait Darstellung */
+            var t = $('#'+container+' .slider-thumbsarea img');
+            var p = height_thumbs.toString()+'px';    // bis mir etwas besseres einfällt
+            t.css('height',p);
+            /* landscape */
+
+        };
 //
 // JAPS
 //
@@ -41,8 +83,13 @@
         var ratio = ratio;             // Seitenverhältnis der Bilddarstellung breite:höhe
         var proz_thumbs = proz_thumbs; // Anteil in % der containerBreite
         var height_thumbs = 72;        // absolute Höhe des Sliders unter dem Bild ( portrait )
+                                       // BESSER: gesteuert durch Anzahl der Bilder im SLider
+                                       //     ==> img width  = floor(Containerbreite/AnzBilder) %
+                                       //     ==> imh height = Breite / ratio
+                                       //            - Höhe derSliderimages ( über padding )
 
         // Die Buchführung
+        var started = false;           // jede Instanz nur einmal starten
         var actualOrientation = 'portrait'; // Orientierung des Viewports!!
         var aktIndex = 0;                   // angezeigtes Bild
 
@@ -50,41 +97,47 @@
         var mainpictureWidthLandscape = ''; // Breiten des Bildfensters, orientierung, proz_thumbs
         var mainpictureWidthPortrait = '100%';
 
-        var that = this;    // ermöglicht Zugriff für private Methoden
-
-        ///////////////////////////////////////////////////////////
-        //
-        // private methods
-        //
-        ///////////////////////////////////////////////////////////
-        function privateMethod() {
-
-        }; // privateMethod
-
-        // Hilfsroutinen
-
-
-
-        function calcWindowWidths() {
-            var b,  // neue breite
-                p   // stringrepresentation
-            b = 100-proz_thumbs;
-            p = b.toString()+'%'
-            mainpictureWidthLandscape = p;
-            // short
-            mainpictureWidthLandscape = (100-proz_thumbs)+'%';
+        var that = this;    // ermöglicht Zugriff für private/innere Methoden ?????
+        // ausprobieren : Zugriff über 'that' geht nicht :(
+        //              : Verpacken in ein options - Objekt ( kommt per Reference )
+        var options = {
+            container : container,
+            ratio     : ratio,
+            mainContainerSelector : '#'+container+' .slider-mainpicture', // 'container' ist der Parameter
+            gates     : 'oder gates nit'
         }
 
-        // Was zu tun ist, wenn this.proz_thumbs sich ändert
-        function calcThumbList() {
-            /* portrait Darstellung */
-            var t = $('#'+container+' .slider-thumbsarea img');
-            var p = height_thumbs.toString()+'px';    // bis mir etwas besseres einfällt
-            t.css('height',p);
-            /* landscape */
 
-        };
+        // function calcWindowWidths() {
+        //     var b,  // neue breite
+        //         p   // stringrepresentation
+        //     b = 100-proz_thumbs;
+        //     p = b.toString()+'%'
+        //     mainpictureWidthLandscape = p;
+        //     // short
+        //     mainpictureWidthLandscape = (100-proz_thumbs)+'%';
+        // }
 
+        // // Was zu tun ist, wenn this.proz_thumbs sich ändert
+        // function calcThumbList() {
+        //     /* portrait Darstellung */
+        //     var t = $('#'+container+' .slider-thumbsarea img');
+        //     var p = height_thumbs.toString()+'px';    // bis mir etwas besseres einfällt
+        //     t.css('height',p);
+        //     /* landscape */
+
+        // };
+
+        function test_inner() {
+            debugger;
+            //var x1 = that.container; Ist 'that' damit sinnlos ?
+            var o = options;
+            var x2 = o.gates;
+            var x3 = 'neu';
+            o.mainContainerSelector = x3;
+            console.log(that);
+            console.log(o);
+        }
         ///////////////////////////////////////////////////////////
         //
         // privileged methods
@@ -105,7 +158,7 @@
         this.setProz_thumbs = function(wert) {
             if ( true ) {
                 proz_thumbs = wert;
-                calcThumbList();
+                calcThumbList(container, height_thumbs, proz_thumbs);
             }
         } // setProz_thumbs
 
@@ -143,16 +196,24 @@
 
         // starter
         this.startUp = function() {
-            // Bildbereich und Sliderbereich setzen
-            // portrait einfach untereinander
-            var bild,       // containerobject picture
-                slider;     // container thumbnail
-            bild = '<div class="slider-mainpicture"></div>';
-            slider = '<div class="slider-thumbsarea"><img src="http://placehold.it/150x50" /></div><div class="clear"></div>';
-            $('#'+container).html(bild+slider);
-            calcWindowWidths();
-            calcRatio();
-            calcThumbList();
+            if ( started === false ) {
+                // Bildbereich und Sliderbereich setzen
+                // portrait einfach untereinander
+                var bild,       // containerobject picture
+                    slider;     // container thumbnail
+
+                test(options);
+                test_inner();
+
+                bild = '<div class="slider-mainpicture"></div>';
+                slider = '<div class="slider-thumbsarea"><img src="http://placehold.it/150x50" /></div><div class="clear"></div>';
+                $('#'+container).html(bild+slider);
+
+                calcWindowWidths(proz_thumbs, mainpictureWidthLandscape);
+                calcRatio(actualOrientation, ratio, container);
+                calcThumbList(container, height_thumbs, proz_thumbs);
+                started = true;
+            } // noch nicht gestartet
         } // startUp
 
     } // end slider
